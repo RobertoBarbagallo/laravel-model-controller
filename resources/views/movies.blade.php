16173cb96f274e0928788970b91bebf5fc8156e3
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-06-22 17:07:00 +0200

    consegna

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -3,15 +3,23 @@
 @section('page_title', 'Movies')
 
 @section('content')
-@foreach($movies as $movie)
-<div class="card">
-    <div class="round">
-        {{$movie->vote}}
+<div class="container">
+    <div class="flex-container">
+
+
+        @foreach($movies as $movie)
+        <div class="card">
+            <div class="round">
+                {{$movie->vote}}
+            </div>
+            <h2>Titolo</h2>
+            <h2>{{$movie->title}}</h2>
+            <h4>Titolo originale</h4>
+            <h4>{{$movie->original_title}}</h4>
+            <p>{{$movie->date}}</p>
+            <p>{{$movie->nationality}}</p>
+        </div>
+        @endforeach
     </div>
-    <h2>Titolo: {{$movie->title}}</h2>
-    <h4>Titolo originale: {{$movie->original_title}}</h4>
-    <p>{{$movie->date}}</p>
-     <p>{{$movie->nationality}}</p>
 </div>
-@endforeach
 @endsection
